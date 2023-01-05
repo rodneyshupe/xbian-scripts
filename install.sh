cd58b84b0ca9e4e838aff0da8f9c6a90bc9be5c9
rodneyshupe <rodney@shupe.ca>
2023-01-05 00:31:37 -0800

    Bug Fixes

--- install.sh
+++ install.sh
@@ -8,22 +8,23 @@ FLAG_FORCE=0
 echo "This script requires sudo permissions."
 sudo true
 
-# Update and install esentials
-sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y install cron htop jq curl zip unzip logrotate mariadb-client
+# Update and install essentials
+echo "Update and Install Essentials"
+sudo apt-get update >/dev/null && sudo apt-get -y upgrade >/dev/null && sudo apt-get -y install cron htop jq curl zip unzip logrotate mariadb-client >/dev/null
 
 # Install rclone
 echo "Install rclone..."
-curl -sS https://rclone.org/install.sh | sudo bash
+curl -sSL https://rclone.org/install.sh | sudo bash
 
 # Install `kodi-rpc`
-echo "Install script..."
+echo "Install kodi-rpc script..."
 wget --quiet --output-document=/tmp/kodi-rpc https://raw.githubusercontent.com/tadly/kodi-rpc/master/kodi-rpc \
   && bash /tmp/kodi-rpc --install \
   && sudo chmod +x /usr/bin/kodi-rpc \
-  && rm /tmp/kodi-rpc
+rm /tmp/kodi-rpc 2>/dev/null
 
 echo "    Write config file..."
-[ ! -f "$HOME/.config/kodi-rpc.conf" ]  && cat > "$HOME/.config/kodi-rpc.conf" <<EOF
+[ ! -f "$HOME/.config/kodi-rpc.conf" ] && cat > "$HOME/.config/kodi-rpc.conf" <<EOF
 # Host/IP the web server is running on
 HOST=localhost
 
@@ -36,7 +37,7 @@ USER="kodi"
 # Password (leave blank if not required)
 PASS="kodi"
 EOF
-sudo cp "$HOME/.config/kodi-rpc.conf" /usr/bin/kodi-rpc.conf
+[ ! -f "/usr/bin/kodi-rpc.conf" ] && sudo cp "$HOME/.config/kodi-rpc.conf" /usr/bin/kodi-rpc.conf
 
 # Create Directories
 echo "Create Directories..."
@@ -49,28 +50,30 @@ sudo mkdir -p /var/log/milliways 2>/dev/null
 
 # Copy Utility Commands
 echo "Copy Utility Commands..."
-curl -sS "$GITHUB_REPO_URL/scripts/restart-kodi.sh" | sudo tee /usr/bin/restart-kodi > /dev/null
-curl -sS "$GITHUB_REPO_URL/scripts/sys-status.sh" | sudo tee /usr/bin/sys-status > /dev/null
-curl -sS "$GITHUB_REPO_URL/scripts/version.sh" | sudo tee /usr/bin/version > /dev/null
+curl -sSL "$GITHUB_REPO_URL/scripts/restart-kodi.sh" | sudo tee /usr/bin/restart-kodi > /dev/null
+curl -sSL "$GITHUB_REPO_URL/scripts/sys-status.sh" | sudo tee /usr/bin/sys-status > /dev/null
+curl -sSL "$GITHUB_REPO_URL/scripts/version.sh" | sudo tee /usr/bin/version > /dev/null
 sudo chmod +x /usr/bin/restart-kodi
 sudo chmod +x /usr/bin/sys-status
 sudo chmod +x /usr/bin/version
 
 # Copy Sample `.env` Files
-echo "Copy Sample `.env` Files..."
-[ ! -f "$HOME/.config/kodi-episode-check.env" ]  && curl "$GITHUB_REPO_URL/sample-configs/kodi-episode-check.env"  "$HOME/.config/kodi-episode-check.env"
-[ ! -f "$HOME/.config/kodi-detail-check.env" ]   && curl "$GITHUB_REPO_URL/sample-configs/kodi-detail-check.env"   "$HOME/.config/kodi-detail-check.env"
-[ ! -f "$HOME/.config/kodi-remove-watched.env" ] && curl "$GITHUB_REPO_URL/sample-configs/kodi-remove-watched.env" "$HOME/.config/kodi-remove-watched.env"
+echo "Copy Sample .env Files..."
+[ ! -f "$HOME/.config/kodi-episode-check.env" ]  && curl -sSL "$GITHUB_REPO_URL/sample-configs/kodi-episode-check.env"  > "$HOME/.config/kodi-episode-check.env"
+[ ! -f "$HOME/.config/kodi-remove-watched.env" ] && curl -sSL "$GITHUB_REPO_URL/sample-configs/kodi-remove-watched.env" > "$HOME/.config/kodi-remove-watched.env"
+[ ! -f "$HOME/.config/sonarr-unmonitor-watched.env" ] && curl -sSL "$GITHUB_REPO_URL/sample-configs/sonarr-unmonitor-watched.env"   > "$HOME/.config/sonarr-unmonitor-watched.env"
 
 # Copy Script Files
 echo "Copy Script Files..."
-curl -sS "$GITHUB_REPO_URL/scripts/kodi-episode-check.sh"  > "$HOME/.scripts/kodi-episode-check.sh"
-curl -sS "$GITHUB_REPO_URL/scripts/kodi-detail-check.sh"   > "$HOME/.scripts/kodi-detail-check.sh"
-curl -sS "$GITHUB_REPO_URL/scripts/kodi-remove-watched.sh" > "$HOME/.scripts/kodi-remove-watched.sh"
-curl -sS "$GITHUB_REPO_URL/scripts/sonarr-unmonitor-watched.sh" > "$HOME/.scripts/sonarr-unmonitor-watched.sh"
-curl -sS "$GITHUB_REPO_URL/scripts/backup.sh"  > "$HOME/.scripts/backup.sh"
-curl -sS "$GITHUB_REPO_URL/scripts/restore.sh" > "$HOME/.scripts/restore.sh"
-curl -sS "$GITHUB_REPO_URL/scripts/pretrip.sh" > "$HOME/.scripts/pretrip.sh"
+curl -sSL "$GITHUB_REPO_URL/scripts/kodi-episode-check.sh"  > "$HOME/.scripts/kodi-episode-check.sh"
+curl -sSL "$GITHUB_REPO_URL/scripts/kodi-detail-check.sh"   > "$HOME/.scripts/kodi-detail-check.sh"
+curl -sSL "$GITHUB_REPO_URL/scripts/kodi-remove-watched.sh" > "$HOME/.scripts/kodi-remove-watched.sh"
+curl -sSL "$GITHUB_REPO_URL/scripts/sonarr-unmonitor-watched.sh" > "$HOME/.scripts/sonarr-unmonitor-watched.sh"
+curl -sSL "$GITHUB_REPO_URL/scripts/backup.sh"  > "$HOME/.scripts/backup.sh"
+curl -sSL "$GITHUB_REPO_URL/scripts/restore.sh" > "$HOME/.scripts/restore.sh"
+curl -sSL "$GITHUB_REPO_URL/scripts/pretrip.sh" > "$HOME/.scripts/pretrip.sh"
+
+chmod +x "$HOME/.scripts"/*
 
 [ -f "$HOME/.scripts/backup.sh" ] && [ ! -f "$HOME/backup" ] && ln -s "$HOME/.scripts/backup.sh" "$HOME/backup"
 [ -f "$HOME/.scripts/restore.sh" ] && [ ! -f "$HOME/restore" ] && ln -s "$HOME/.scripts/restore.sh" "$HOME/restore"
@@ -83,9 +86,9 @@ echo '#!/bin/bash' | sudo tee /etc/update-motd.d/14-xbian > /dev/null \
     && cat /etc/motd.ori | sudo tee --append /etc/update-motd.d/14-xbian > /dev/null \
     && echo "'" | sudo tee --append /etc/update-motd.d/14-xbian > /dev/null \
     && sudo chmod +x /etc/update-motd.d/14-xbian \
-    && sudo rm /etc/motd
+    && [ -f /etc/motd  ] && sudo rm /etc/motd
 
-curl -sS "$GITHUB_REPO_URL/scripts/update-motd.d/15-logo.sh" | sudo tee /etc/update-motd.d/15-logo > /dev/null
-curl -sS "$GITHUB_REPO_URL/scripts/update-motd.d/20-status.sh" | sudo tee /etc/update-motd.d/20-status > /dev/null
+curl -sSL "$GITHUB_REPO_URL/scripts/update-motd.d/15-logo.sh" | sudo tee /etc/update-motd.d/15-logo > /dev/null
+curl -sSL "$GITHUB_REPO_URL/scripts/update-motd.d/20-status.sh" | sudo tee /etc/update-motd.d/20-status > /dev/null
 sudo chmod +x /etc/update-motd.d/15-logo
 sudo chmod +x /etc/update-motd.d/20-status
