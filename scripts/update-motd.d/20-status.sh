be8b903981319e3dfbe7fbf6822afc59820c4c45
rodneyshupe <rodney@shupe.ca>
2023-01-06 23:45:54 -0800

    fixed comment

--- scripts/update-motd.d/20-status.sh
+++ scripts/update-motd.d/20-status.sh
@@ -3,7 +3,7 @@
 #    20-status - display status
 #
 
-# To be stored in /etc/update-motd.d/15-logo
+# To be stored in /etc/update-motd.d/20-status
 
 export TERM=xterm-256color
 /usr/local/bin/sys-status
